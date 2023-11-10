811e14c51b1002534e814c8c5a2f9658ffc52af5
SiroSchochEB <siro.schoch@bluewin.ch>
2023-11-10 15:36:39 +0100

    Simple player movment offline
    Geils füdli

---
+++ Scardy/Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+public class PlayerMovment : MonoBehaviour
+{
+    public float moveSpeed;
+	private Rigidbody myRigidbody;
+
+	private Vector3 moveInput;
+	private Vector3 moveVelocity;
+
+    private Camera mainCamera;
+    private Vector2 move;
+
+    public void OnMove(InputAction.CallbackContext context)
+	{
+		move = context.ReadValue<Vector2>();
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody> ();
+		mainCamera = FindObjectOfType<Camera> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0f, Input.GetAxisRaw ("Vertical")); //movment
+		moveVelocity = moveInput * moveSpeed;
+        movePlayer();
+    }
+
+    public void movePlayer() {
+
+		Vector3 movment = new Vector3 (move.x, 0f, move.y); //movment
+
+		//transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movment), 0.15f);
+
+		transform.Translate(movment * moveSpeed * Time.deltaTime, Space.World);
+	}
+}
